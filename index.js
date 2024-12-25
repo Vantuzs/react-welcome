ee62eeaf5e8aa5b1c320ce8c934b38647447e4ff
Vantuz <Shcherbina444@gmail.com>
2024-12-25 19:35:22 +0200

    Class component

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+class HeaderComponent extends React.Component {
+    anotherMethod(){ // Другоие какието всередине компонента не запрещено(но метод render это обязательно)
+
+    }
+
+    render(){ // единственный метод, который вы должны вызначить в класе
+        const p = React.createElement('p',{},'Super paragraph');
+        const h2 = React.createElement('h2',{title: 'Hi!',Agugugu: 'Suuu'},'Header');
+        const article = React.createElement('article',{},h2,p);
+        return article;
+    }
+}
+
+const component = React.createElement(HeaderComponent);
+
+
+
+
 
 
 const root = document.querySelector('#root');
-const h1 = React.createElement('h1',{},'Hello React');// React-element
-console.log(h1);
 
-const anotherH1 = document.createElement('h1'); // HTML-element
-console.dir(anotherH1);
+ReactDOM.render(component,root)
 
-ReactDOM.render(h1,root);
+
+
+
+
+/* 
+
+Подключить реакт и Реакт-ДОМ
+СОздайте елемент и прикрепите его на страничку
+
+
+*/
