b371192bf8080d0e8d9faaede0f7c4b52b32d9cc
Vantuz <Shcherbina444@gmail.com>
2024-12-26 14:04:37 +0200

    Task practise 060 resolve

--- index.js
+++ index.js
@@ -2,21 +2,48 @@ class Counter extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            count: 0
+            count: 0,
+            shag: 1
         };
     }
 
     increment(){
         // todo: state.cout++;
         this.setState({
-            count: this.state.count + 1
+            count: this.state.count + this.state.shag
         })
+    }
+
+    decrement(){
+        if(this.state.count < this.state.shag){
+            return this.setState({
+                count: this.state.count = 0
+            })
+        }else{
+        this.setState({
+           count: this.state.count - this.state.shag
+        })}
+    }
+
+    upShag(){
+        if(this.state.shag >=5){
+            this.setState({
+                shag: this.state.shag = 1
+            })
+        }else{
+            this.setState({
+                shag: this.state.shag + 1
+            })
+        }
     }
 
     render(){
         const h2 = React.createElement('h2',{},this.state.count);
+        const shagVui = React.createElement('h2',{},this.state.shag,' шаг');
         const btn = React.createElement('button',{onClick: ()=>{this.increment()}},'+');
-        return React.createElement(React.Fragment,{},h2,btn);
+        const btnMin = React.createElement('button',{onClick: ()=>{this.decrement()}},'-');
+        const btnVal = React.createElement('button',{onClick: ()=>{this.upShag()}},'+1shag')
+        return React.createElement(React.Fragment,{},h2,shagVui,btn,btnMin,btnVal);
     }
 }
 
@@ -26,9 +53,39 @@ const root = document.querySelector('#root');
 
 ReactDOM.render(component,root)
 
+class Couter2 extends React.Component{
+    constructor(promps){
+        super(promps);
+        this.state = {
+            care: 1
+        }
+    }
+
+    increment(){
+        if(this.state.cate === 10){
+            return 0;
+        }else{
+        this.setState({
+            care: this.state.care + 1
+        })
+    }}
+
+    decrement(){
+
+    }
+        
+}
 
 
 
+/* 
+
+1. Сделайте такой же couter, который кроме инкремента будет уметь еще и декрементовать личильник
+
+
+
+
+*/
 
 /* 
 
